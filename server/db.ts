a907fba2bd74a6502f435e6eb90bb998f7be7b96
checodovalina <41979438-checodovalina@users.noreply.replit.com>
2025-08-08 00:13:28 +0000

    Restored to 'e7107f0dd90f14bcaccd13258db231b8e2380482'
    Replit-Restored-To: e7107f0dd90f14bcaccd13258db231b8e2380482

--- server/db.ts
+++ server/db.ts
@@ -1,6 +1,9 @@
-import { Client } from 'pg';
-import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool, neonConfig } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-serverless';
+import ws from "ws";
 import * as schema from "@shared/schema";
+
+neonConfig.webSocketConstructor = ws;
 
 if (!process.env.DATABASE_URL) {
   throw new Error(
@@ -8,6 +11,5 @@ if (!process.env.DATABASE_URL) {
   );
 }
 
-export const client = new Client({ connectionString: process.env.DATABASE_URL });
-client.connect();
-export const db = drizzle(client, { schema });
+export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const db = drizzle({ client: pool, schema });
